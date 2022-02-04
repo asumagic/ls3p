a36c33a953b0b967ecc13d2ac07610f6d9e85cb4
Asu <sdelang@sdelang.fr>
2022-02-04 22:51:32 +0100

    Added ResponseMessage tests

--- tests/jsontests/basemessages.cpp
+++ tests/jsontests/basemessages.cpp
@@ -1,5 +1,7 @@
 #include "ls3p/messages/base/ProgressParams.hpp"
 #include "ls3p/messages/base/RequestMessage.hpp"
+#include "ls3p/messages/base/ResponseMessage.hpp"
+#include "ls3p/util/exceptions.hpp"
 #include <catch2/catch.hpp>
 
 #include <ls3p/messages/base.hpp>
@@ -48,4 +50,55 @@ TEST_CASE("Test RequestMessage parsing", "[request]") {
     REQUIRE(std::get<int>(message.id) == 123);
     REQUIRE(message.method == "foobar");
     REQUIRE(!message.params.has_value());
+}
+
+TEST_CASE("Test ResponseMessage (payload case) parsing", "[request]") {
+    const auto message = json::parse(R"(
+        {
+            "id": 123,
+            "params": 456
+        }
+    )").get<base::ResponseMessage>();
+
+    REQUIRE(std::get<int>(message.id) == 123);
+    REQUIRE(std::get<int>(message.params.value()) == 456);
+    REQUIRE(!message.error.has_value());
+}
+
+TEST_CASE("Test ResponseMessage (params+error fields) exception", "[request]") {
+    REQUIRE_THROWS_AS(
+        json::parse(R"(
+            {
+                "id": 123,
+                "params": 456,
+                "error": {
+                    "code": -32601,
+                    "message": "oh noes an error >w<"
+                }
+            }
+        )").get<base::ResponseMessage>(),
+        ls3p::util::ParsingException
+    );
+}
+
+TEST_CASE("Test ResponseMessage (+ResponseError) parsing", "[request]") {
+    const auto message = json::parse(R"(
+        {
+            "id": 123,
+            "error": {
+                "code": -32601,
+                "message": "oh noes an error >w<",
+                "data": {
+                    "details": "that error code is large oh no"
+                }
+            }
+        }
+    )").get<base::ResponseMessage>();
+
+    REQUIRE(std::get<int>(message.id) == 123);
+
+    const auto& err = message.error.value();
+    REQUIRE(err.code == -32601);
+    REQUIRE(err.message == "oh noes an error >w<");
+    REQUIRE(std::get<nlohmann::json>(err.data.value()).at("details") == "that error code is large oh no");
 }
