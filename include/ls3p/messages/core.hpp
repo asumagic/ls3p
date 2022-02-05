f82db9da8bbc87ee2d805cfc1b0cdea43e7bb590
Asu <sdelang@sdelang.fr>
2022-02-05 14:29:51 +0100

    Introduce core.hpp

---
+++ include/ls3p/messages/core.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "core/command.hpp"
+#include "core/diagnostic.hpp"
+#include "core/location.hpp"
+#include "core/markup.hpp"
+#include "core/progress.hpp"
+#include "core/registration.hpp"
+#include "core/textdocument.hpp"
+#include "core/textedit.hpp"
+#include "core/trace.hpp"
+#include "core/workspaceedit.hpp"
