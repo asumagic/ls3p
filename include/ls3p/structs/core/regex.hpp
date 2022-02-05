a9919ceceed25bc1c6efacc1e7575d485dd6164e
Asu <sdelang@sdelang.fr>
2022-02-05 19:14:29 +0100

    Implement LS3P_ARCHIVE for regex.hpp

--- include/ls3p/structs/core/regex.hpp
+++ include/ls3p/structs/core/regex.hpp
@@ -9,6 +9,13 @@ struct RegularExpressionsClientCapabilities
 {
     std::string engine;
     std::optional<std::string> version;
+
+    LS3P_ARCHIVE(RegularExpressionsClientCapabilities)
+    {
+        archive
+            ("engine", engine)
+            ("version", version);
+    }
 };
 
 }
