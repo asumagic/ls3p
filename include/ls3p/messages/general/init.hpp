887a97a1000dcc7d38d3badfbfc9fdfd52e423b0
Asu <sdelang@sdelang.fr>
2022-02-05 14:43:11 +0100

    Move some client caps stuff to init.hpp

--- include/ls3p/messages/general/init.hpp
+++ include/ls3p/messages/general/init.hpp
@@ -2,9 +2,70 @@
 
 #include <ls3p/messages/common.hpp>
 #include <ls3p/messages/caps/ClientCapabilities.hpp>
+#include <variant>
 
 namespace ls3p::messages::general
 {
+
+struct TextDocumentClientCapabilities
+{
+    // TODO: synchronization
+    // TODO: completion
+    // TODO: hover
+    // TODO: signatureHelp
+    // TODO: declaration
+    // TODO: definition
+    // TODO: typeDefinition
+    // TODO: implementation
+    // TODO: references
+    // TODO: documentHighlight
+    // TODO: documentSymbol
+    // TODO: codeAction
+    // TODO: codeLens
+    // TODO: documentLink
+    // TODO: colorProvider
+    // TODO: formatting
+    // TODO: rangeFormatting
+    // TODO: onTypeFormatting
+    // TODO: rename
+    // TODO: publishDiagnostics
+    // TODO: foldingRange
+    // TODO: selectionRange
+    // TODO: linkedEditingRange
+    // TODO: callHierarchy
+    // TODO: semanticTokens
+    // TODO: moniker
+};
+
+struct ClientCapabilities
+{
+    struct Workspace
+    {
+        std::optional<bool> apply_edit;
+
+        // TODO: workspaceEdit
+        // TODO: didChangeConfiguration
+        // TODO: didChangeWatchedFiles
+        // TODO: symbol
+        // TODO: executeCommand
+        // TODO: workspaceFolders
+        // TODO: configuration
+        // TODO: semanticTokens
+        // TODO: codeLens
+        // TODO: fileOperations
+    };
+
+    std::optional<Workspace> workspace;
+
+    std::optional<TextDocumentClientCapabilities> text_document;
+
+    struct Window
+    {
+        std::optional<bool> work_done_progress;
+
+        
+    };
+};
 
 struct InitializeParams
 {
@@ -21,7 +82,10 @@ struct InitializeParams
 
     std::optional<std::string> locale;
 
-    nlohmann::json initialization_options;
+    [[deprecated("root_uri is recommended in the LSP")]]
+    std::optional<std::variant<std::string, std::monostate>> root_path;
+
+    std::optional<nlohmann::json> initialization_options;
 
     ClientCapabilities capabilities;
 
