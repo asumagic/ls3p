10d9fc0084c86e89f15f494e8dfc91f8450f0928
Asu <sdelang@sdelang.fr>
2022-02-05 19:48:05 +0100

    Implement LS3P_ARCHIVE for workspaceedit.hpp

--- include/ls3p/structs/core/workspaceedit.hpp
+++ include/ls3p/structs/core/workspaceedit.hpp
@@ -5,49 +5,6 @@
 namespace ls3p::structs::core
 {
 
-struct CreateFileOptions
-{
-    std::optional<bool> overwrite;
-    std::optional<bool> ignore_if_exists;
-};
-
-struct CreateFile
-{
-    // NOTE: must assert on kind == "create"
-    DocumentUri uri;
-    std::optional<CreateFileOptions> options;
-    std::optional<ChangeAnnotationIdentifier> annotation_id;
-};
-
-struct RenameFileOptions
-{
-    std::optional<bool> overwrite;
-    std::optional<bool> ignore_if_exists;
-};
-
-struct RenameFile
-{
-    // NOTE: must assert on kind == "rename"
-    DocumentUri old_uri;
-    DocumentUri new_uri;
-    std::optional<RenameFileOptions> options;
-    std::optional<ChangeAnnotationIdentifier> annotation_id;
-};
-
-struct DeleteFileOptions
-{
-    std::optional<bool> overwrite;
-    std::optional<bool> ignore_if_not_exists;
-};
-
-struct DeleteFile
-{
-    // NOTE: must assert on kind == "delete"
-    DocumentUri uri;
-    std::optional<DeleteFileOptions> options;
-    std::optional<ChangeAnnotationIdentifier> annotation_id;
-};
-
 namespace resource_operation_kind
 {
 
@@ -60,6 +17,96 @@ static constexpr std::string_view
 
 /// \see resource_operation_kind
 using ResourceOperationKind = std::string;
+
+struct CreateFileOptions
+{
+    std::optional<bool> overwrite;
+    std::optional<bool> ignore_if_exists;
+
+    LS3P_ARCHIVE(CreateFileOptions)
+    {
+        archive
+            ("overwrite", overwrite)
+            ("ignoreIfExists", ignore_if_exists);
+    }
+};
+
+struct CreateFile
+{
+    DocumentUri uri;
+    std::optional<CreateFileOptions> options;
+    std::optional<ChangeAnnotationIdentifier> annotation_id;
+
+    LS3P_ARCHIVE(CreateFile)
+    {
+        archive
+            .static_field("kind", resource_operation_kind::CREATE)
+            ("uri", uri)
+            ("options", options)
+            ("annotationId", annotation_id);
+    }
+};
+
+struct RenameFileOptions
+{
+    std::optional<bool> overwrite;
+    std::optional<bool> ignore_if_exists;
+
+    LS3P_ARCHIVE(RenameFileOptions)
+    {
+        archive
+            ("overwrite", overwrite)
+            ("ignoreIfExists", ignore_if_exists);
+    }
+};
+
+struct RenameFile
+{
+    DocumentUri old_uri;
+    DocumentUri new_uri;
+    std::optional<RenameFileOptions> options;
+    std::optional<ChangeAnnotationIdentifier> annotation_id;
+
+    LS3P_ARCHIVE(RenameFile)
+    {
+        archive
+            .static_field("kind", resource_operation_kind::RENAME)
+            ("oldUri", old_uri)
+            ("newUri", new_uri)
+            ("options", options)
+            ("annotaionId", annotation_id);
+    }
+};
+
+struct DeleteFileOptions
+{
+    std::optional<bool> overwrite;
+    std::optional<bool> ignore_if_not_exists;
+
+    LS3P_ARCHIVE(DeleteFileOptions)
+    {
+        archive
+            ("overwrite", overwrite)
+            ("ignoreIfNotExists", ignore_if_not_exists);
+    }
+};
+
+struct DeleteFile
+{
+    // NOTE: must assert on kind == "delete"
+    DocumentUri uri;
+    std::optional<DeleteFileOptions> options;
+    std::optional<ChangeAnnotationIdentifier> annotation_id;
+
+    LS3P_ARCHIVE(DeleteFile)
+    {
+        archive
+            .static_field("kind", resource_operation_kind::DELETE)
+            ("uri", uri)
+            ("options", options)
+            ("annotationId", annotation_id);
+    }
+};
 
 namespace failure_handling_kind
 {
@@ -77,7 +124,13 @@ using FailureHandlingKind = std::string;
 
 struct ChangeAnnotationSupport
 {
-    std::optional<bool> groups_on_labels;
+    std::optional<bool> groups_on_label;
+
+    LS3P_ARCHIVE(ChangeAnnotationSupport)
+    {
+        archive
+            ("groupsOnLabel", groups_on_label);
+    }
 };
 
 struct WorkspaceEdit
@@ -89,11 +142,31 @@ struct WorkspaceEdit
         std::optional<std::vector<FailureHandlingKind>> failure_handling;
         std::optional<bool> normalizes_line_endings;
         std::optional<ChangeAnnotationSupport> change_annotation_support;
+
+        LS3P_ARCHIVE(ClientCapabilities)
+        {
+            archive
+                ("documentChanges", document_changes)
+                ("resourceOperations", resource_operations)
+                ("failureHandling", failure_handling)
+                ("normalizesLineEndings", normalizes_line_endings)
+                ("changeAnnotationSupport", change_annotation_support);
+        }
     };
 
     std::optional<std::map<DocumentUri, std::vector<TextEdit>>> changes;
 
     std::vector<std::variant<TextDocumentEdit, CreateFile, RenameFile, DeleteFile>> document_changes;
+
+    std::optional<std::map<ChangeAnnotationIdentifier, ChangeAnnotation>> change_annotations;
+
+    LS3P_ARCHIVE(WorkspaceEdit)
+    {
+        archive
+            ("changes", changes)
+            ("documentChanges", document_changes)
+            ("changeAnnotations", change_annotations);
+    }
 };
 
 }
