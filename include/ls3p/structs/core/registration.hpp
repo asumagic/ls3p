2f6146c324d6fd60af59bc04a4f5016c708374a1
Asu <sdelang@sdelang.fr>
2022-02-05 19:15:55 +0100

    Implement LS3P_ARCHIVE for registration.hpp

--- include/ls3p/structs/core/registration.hpp
+++ include/ls3p/structs/core/registration.hpp
@@ -11,6 +11,14 @@ struct DocumentFilter
     std::optional<std::string> language;
     std::optional<std::string> scheme;
     std::optional<std::string> pattern;
+
+    LS3P_ARCHIVE(DocumentFilter)
+    {
+        archive
+            ("language", language)
+            ("scheme", scheme)
+            ("pattern", pattern);
+    }
 };
 
 using DocumentSelector = std::vector<DocumentFilter>;
@@ -18,11 +26,23 @@ using DocumentSelector = std::vector<DocumentFilter>;
 struct TextDocumentRegistrationOptions
 {
     std::variant<DocumentSelector, std::monostate> document_selector;
+
+    LS3P_ARCHIVE(TextDocumentRegistrationOptions)
+    {
+        archive
+            ("documentSelector", document_selector);
+    }
 };
 
 struct StaticRegistrationOptions
 {
     std::optional<std::string> id;
+
+    LS3P_ARCHIVE(StaticRegistrationOptions)
+    {
+        archive
+            ("id", id);
+    }
 };
 
 }
