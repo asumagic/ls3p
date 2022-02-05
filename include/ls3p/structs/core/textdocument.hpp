a09e2b716f7031c8e3204d2cb8274dc26ce25bf2
Asu <sdelang@sdelang.fr>
2022-02-05 19:18:29 +0100

    Implement LS3P_ARCHIVE for textdocument.hpp

--- include/ls3p/structs/core/textdocument.hpp
+++ include/ls3p/structs/core/textdocument.hpp
@@ -9,16 +9,34 @@ namespace ls3p::structs::core
 struct TextDocumentIdentifier
 {
     DocumentUri uri;
+
+    LS3P_ARCHIVE(TextDocumentIdentifier)
+    {
+        archive
+            ("uri", uri);
+    }
 };
 
 struct OptionalVersionedTextDocumentIdentifier : TextDocumentIdentifier
 {
     std::variant<Integer, std::monostate> version;
+
+    LS3P_ARCHIVE(OptionalVersionedTextDocumentIdentifier)
+    {
+        archive
+            ("version", version);
+    }
 };
 
 struct VersionedTextDocumentIdentifier : TextDocumentIdentifier
 {
     Integer version;
+
+    LS3P_ARCHIVE(VersionedTextDocumentIdentifier)
+    {
+        archive
+            ("version", version);
+    }
 };
 
 struct TextDocumentItem
@@ -27,12 +45,28 @@ struct TextDocumentItem
     std::string language_id;
     Integer version;
     std::string text;
+
+    LS3P_ARCHIVE(TextDocumentItem)
+    {
+        archive
+            ("uri", uri)
+            ("languageId", language_id)
+            ("version", version)
+            ("text", text);
+    }
 };
 
 struct TextDocumentPositionParams
 {
     TextDocumentIdentifier text_document;
     Position position;
+
+    LS3P_ARCHIVE(TextDocumentPositionParams)
+    {
+        archive
+            ("textDocument", text_document)
+            ("position", position);
+    }
 };
 
 }
