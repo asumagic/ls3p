3f6e02e645c0b24ae25ad4583e57e1ba4d768807
Asu <sdelang@sdelang.fr>
2022-02-05 19:20:35 +0100

    Added includeall.cpp to check for compile errors in some headers

---
+++ tests/jsontests/includeall.cpp
@@ -0,0 +1,6 @@
+// Not a true test; includes ~all headers that may be include-only to validate whether any compile error occurs
+
+#include <ls3p/structs/base.hpp>
+#include <ls3p/structs/core.hpp>
+#include <ls3p/structs/general.hpp>
+#include <ls3p/structs/workspace.hpp>
