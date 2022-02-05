23d79d74150e7cd920eff4426a606b4af7b8c6a3
Asu <sdelang@sdelang.fr>
2022-02-05 19:38:49 +0100

    Implement LS3P_ARCHIVE for textedit.hpp

--- include/ls3p/structs/core/textedit.hpp
+++ include/ls3p/structs/core/textedit.hpp
@@ -12,17 +12,38 @@ struct ChangeAnnotation
     std::string label;
     std::optional<bool> needs_confirmation;
     std::optional<std::string> description;
+
+    LS3P_ARCHIVE(ChangeAnnotation)
+    {
+        archive
+            ("label", label)
+            ("needsConfirmation", needs_confirmation)
+            ("description", description);
+    }
 };
 
 struct TextEdit
 {
     Range range;
     std::string new_text;
+
+    LS3P_ARCHIVE(TextEdit)
+    {
+        archive
+            ("range", range)
+            ("newText", new_text);
+    }
 };
 
 struct AnnotatedTextEdit : TextEdit
 {
     ChangeAnnotationIdentifier annotation_id;
+
+    LS3P_ARCHIVE(AnnotatedTextEdit)
+    {
+        archive
+            ("annotationId", annotation_id);
+    }
 };
 
 struct TextDocumentEdit
@@ -33,6 +54,13 @@ struct TextDocumentEdit
         std::vector<TextEdit>,
         std::vector<AnnotatedTextEdit>
     > edits;
+
+    LS3P_ARCHIVE(TextDocumentEdit)
+    {
+        archive
+            ("textDocument", text_document)
+            ("edits", edits);
+    }
 };
 
 }
